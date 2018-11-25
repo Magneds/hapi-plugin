f2e9f781b3ced73f1627dec993c3773f764043dd
Rogier Spieker <rogier.spieker@magneds.com>
2018-11-25 11:17:55 +0100

    Added means to provide a more elaborate plugin configuration

--- source/index.js
+++ source/index.js
@@ -1,59 +1,96 @@
 const storage = new WeakMap();
 
 class HapiPlugin {
-	constructor(options) {
-		storage.set(this, { options })
+	constructor(config = {}) {
+		storage.set(this, { config })
 	}
 
-	get options() {
-		const { options } = storage.get(this);
+	get config() {
+		const { config } = storage.get(this);
 
-		return options;
+		return config;
 	}
 
 
 	get name() {
-		const { name } = this.options;
+		const { name } = this.config;
 
 		return name;
 	}
 
 	set name(value) {
-		this.options.name = value;
+		this.config.name = value;
 	}
 
 
 	get version() {
-		const { version } = this.options;
+		const { version } = this.config;
 
 		return version;
 	}
 
 	set version(value) {
-		this.options.version = value;
+		this.config.version = value;
 	}
 
-
 	get register() {
-		const { register } = this.options;
-		const invoke = register || function () {
+		const { register } = this.config;
+		const invoke = register ? register : async () => {
 			throw new Error(`HapiPlugin "${this.name}" has no register method`);
 		};
 
-		return async (server, options) => invoke(server, options);
+		return invoke;
 	}
 
 	set register(value) {
-		this.options.register = value;
+		this.config.register = value;
 	}
 
-
 	get dependencies() {
-		return this.options.dependencies || [];
+		return this.config.dependencies || [];
 	}
 
 	set dependencies(value) {
-		this.options.dependencies = value;
+		this.config.dependencies = value;
+	}
+
+	get options() {
+		return this.config.options || {};
+	}
+
+	set options(value) {
+		this.config.options = value || {};
+	}
+
+	get routes() {
+		return this.config.routes;
+	}
+
+	set routes(value) {
+		this.config.routes = value;
+	}
+
+	get prefix() {
+		return (this.routes || {}).prefix;
+	}
+
+	set prefix(value) {
+		this.routes = { ...(this.routes || {}), prefix: value };
+	}
+
+	get export() {
+		const { name, version, dependencies: depends, options, register, routes } = this;
+		const dependencies = depends.length ? depends : undefined;
+
+		if (routes || options) {
+			return {
+				plugin: { name, version, dependencies, register },
+				options,
+				routes,
+			};
+		}
+
+		return { name, version, dependencies, register };
 	}
 }
 
